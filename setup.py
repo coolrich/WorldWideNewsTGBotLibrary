9c63c28ab8615dd0f3a99ec760b018a245da1100
CraftBox <i.domnich2017@gmail.com>
2023-12-21 12:05:16 +0200

    - Created and added files to git. - Created a module wwn-tgbot-lib. - Created setup.py file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='WorldWideNewsTGBotLibrary',
+    version='',
+    packages=['wwn-tgbot-lib'],
+    url='',
+    license='',
+    author='CraftBox',
+    author_email='',
+    description=''
+)
